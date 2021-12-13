6158ec61e1a1bca4eff0c8ae30e9cf5bffce225b
Samkelo <joymama078@gmail.com>
2021-12-13 16:58:58 +0200

    Finalise navbar layout

--- vetkoek/src/components/navbar/index.tsx
+++ vetkoek/src/components/navbar/index.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-        <div className="flex sticky flex-row py-1 px-4 mb-7 w-full">
+        <div className="sticky top-0 justify-center flex flex-row pt-3 pl-5 pb-4 w-full">
             <a title="Search" href="#" className="material-icons text-gray-400 hover:text-white mr-8">search</a>
             <a href="/" className="text-normal mr-7 font-bold text-gray-400 hover:text-white">Frontpage</a>
             <a href="/" className="text-normal mr-7 font-bold text-gray-400 hover:text-white">Upvoted</a>
