1d1899c9508946032ccf4dc26dbfac75de454a1f
Samkelo <joymama078@gmail.com>
2021-12-14 16:41:58 +0200

    Use react native components in nav links

--- vetkoek/src/components/Navbar.tsx
+++ vetkoek/src/components/Navbar.tsx
@@ -1,20 +1,21 @@
-import { Fragment } from "react"
+import { View, Text } from "react-native"
 import { Link } from "react-router-dom"
+import { styles } from "./style"
 
 const Navbar = () => {
     return (
-        <Fragment>
+        <View>
             <Link to="/" className="flex rounded-3xl mt-7 pl-3 py-3 navlink">
                 <i className="material-icons-outlined pl-3">home</i>
-                <span className="text-xl ml-6 -mt-1">Home</span>
+                <Text style={styles.navLinkText}>Home</Text>
             </Link>
             <Link to="/explore" className="flex rounded-3xl px-3 py-3 navlink">
                 <i className="material-icons-outlined pl-3">explore</i>
-                <span className="text-xl ml-6 -mt-1">Explore</span>
+                <Text style={styles.navLinkText}>Explore</Text>
             </Link>
             <Link to="/blog" className="flex rounded-3xl px-3 py-3 navlink">
                 <i className="material-icons-outlined pl-3">article</i>
-                <span className="text-xl ml-6 -mt-1">Blog</span>
+                <Text style={styles.navLinkText}>Blog</Text>
             </Link>
             <Link to="/community" className="flex rounded-3xl px-3 py-3 navlink">
                 <i className="material-icons-outlined pl-3">forums</i>
@@ -22,13 +23,13 @@ const Navbar = () => {
             </Link>
             <Link to="/account" className="flex rounded-3xl px-3 py-3 navlink">
                 <i className="material-icons-outlined pl-3">account_circle</i>
-                <span className="text-xl ml-6 -mt-1">Profile</span>
+                <Text style={styles.navLinkText}>Profile</Text>
             </Link>
             <Link to="/about" className="flex rounded-3xl px-3 py-3 navlink">
                 <i className="material-icons-outlined pl-3">info</i>
-                <span className="text-xl ml-6 -mt-1">About</span>
+                <Text style={styles.navLinkText}>About</Text>
             </Link>
-        </Fragment>
+        </View>
     )
 }
 
--- vetkoek/src/components/style.ts
+++ vetkoek/src/components/style.ts
@@ -1,18 +1,24 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    logoContainer:{
+    logoContainer: {
         marginTop: "1rem",
     },
-    logoPNG:{
-        width:50,
+    logoPNG: {
+        width: 50,
     },
-    logoText:{
+    logoText: {
         fontWeight: "700",
         fontSize: 30,
-        color:"#fff",
+        color: "#fff",
         fontFamily: "IM Fell English SC",
         marginVertical: "0.75rem"
 
+    },
+    navLinkText: {
+        fontSize: 20,
+        marginLeft: "1.5rem",
+        marginTop: "-0.25rem",
+        color: "#fff",
     }
 })
