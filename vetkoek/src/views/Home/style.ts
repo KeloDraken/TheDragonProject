33bf1215bee45a6ccec78387e0130ca6b35b4571
Samkelo <joymama078@gmail.com>
2021-12-14 16:46:41 +0200

    Code clean up

--- vetkoek/src/views/Home/style.ts
+++ vetkoek/src/views/Home/style.ts
@@ -2,7 +2,6 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     pageHeading:{
-        // "font-bold text-xl"
         fontWeight: "700",
         fontSize: 20,
         color: "#fff"
