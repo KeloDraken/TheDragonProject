9ab94692e06fed427c2966bc99dc16501b67e969
Samkelo <joymama078@gmail.com>
2021-12-14 16:18:07 +0200

    Change font sizes in sidebar cards

--- vetkoek/src/components/ArticlesList.tsx
+++ vetkoek/src/components/ArticlesList.tsx
@@ -5,22 +5,22 @@ const ArticlesList = () => {
     return (
         <Fragment>
             <article className="explore_card mx-3 pt-4 rounded-2xl">
-                <h1 className="font-bold text-2xl px-4 mb-4">Card Title</h1>
-                <div className="link_hover px-5 py-4">
+                <h1 className="font-bold text-xl px-4 mb-4">Card Title</h1>
+                <div className="link_hover px-5 py-3">
                     <p className="text-sm text-gray-400">Posted in <span className="underline cursor-pointer">Entrepreneurship</span></p>
-                    <p className="font-bold text-lg">Article one</p>
+                    <p className="font-bold text-normal">Article one</p>
                 </div>
-                <div className="link_hover px-5 py-4">
+                <div className="link_hover px-5 py-3">
                     <p className="text-sm text-gray-400">Posted in <span className="underline cursor-pointer">Job opportunities</span></p>
-                    <p className="font-bold text-lg">Article two</p>
+                    <p className="font-bold text-normal">Article two</p>
                 </div>
-                <div className="link_hover px-5 py-4">
+                <div className="link_hover px-5 py-3">
                     <p className="text-sm text-gray-400">Posted in <span className="underline cursor-pointer">Side projects</span></p>
-                    <p className="font-bold text-lg">Article three</p>
+                    <p className="font-bold text-normal">Article three</p>
                 </div>
-                <div className="link_hover px-5 py-4">
+                <div className="link_hover px-5 py-3">
                     <p className="text-sm text-gray-400">Posted in <span className="underline cursor-pointer">SEO</span></p>
-                    <p className="font-bold text-lg">Article four</p>
+                    <p className="font-bold text-normal">Article four</p>
                 </div>
                 <Link to="/blog" className="text-red-300">
                     <div className="link_hover px-5 rounded-b-2xl py-5">
--- vetkoek/src/components/UpcomingProjects.tsx
+++ vetkoek/src/components/UpcomingProjects.tsx
@@ -5,20 +5,20 @@ const UpcomingProjects = () => {
     return (
         <Fragment>
             <article className="explore_card mx-3 pt-4 rounded-2xl mt-5">
-                <h1 className="font-bold text-2xl px-4 mb-4">Card Title</h1>
-                <div className="link_hover px-5 py-4">
+                <h1 className="font-bold text-xl px-4 mb-4">Card Title</h1>
+                <div className="link_hover px-5 py-3">
                     <p className="text-sm text-gray-400">Posted by <span className="underline cursor-pointer">user1</span></p>
                     <p className="font-bold text-lg">Product one</p>
                 </div>
-                <div className="link_hover px-5 py-4">
+                <div className="link_hover px-5 py-3">
                     <p className="text-sm text-gray-400">Posted by <span className="underline cursor-pointer">user2</span></p>
                     <p className="font-bold text-lg">Product two</p>
                 </div>
-                <div className="link_hover px-5 py-4">
+                <div className="link_hover px-5 py-3">
                     <p className="text-sm text-gray-400">Posted by <span className="underline cursor-pointer">user3</span></p>
                     <p className="font-bold text-lg">Product three</p>
                 </div>
-                <div className="link_hover px-5 py-4 rounded-b-2xl">
+                <div className="link_hover px-5 py-3 rounded-b-2xl">
                     <p className="text-sm text-gray-400">Posted by <span className="underline cursor-pointer">user4</span></p>
                     <p className="font-bold text-lg">Product four</p>
                 </div>
