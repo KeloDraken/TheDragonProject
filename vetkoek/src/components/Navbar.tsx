d7b7e965be228bec66573cc3a394e543f709aa9c
Samkelo <joymama078@gmail.com>
2021-12-14 15:50:29 +0200

    Add routes in links

--- vetkoek/src/components/Navbar.tsx
+++ vetkoek/src/components/Navbar.tsx
@@ -1,32 +1,33 @@
 import { Fragment } from "react"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
         <Fragment>
-            <a href="#" className="flex rounded-3xl mt-7 pl-3 py-3 navlink">
+            <Link to="/" className="flex rounded-3xl mt-7 pl-3 py-3 navlink">
                 <i className="material-icons-outlined pl-3">home</i>
                 <span className="text-xl ml-6 -mt-1">Home</span>
-            </a>
-            <a href="#" className="flex rounded-3xl px-3 py-3 navlink">
+            </Link>
+            <Link to="/explore" className="flex rounded-3xl px-3 py-3 navlink">
                 <i className="material-icons-outlined pl-3">explore</i>
                 <span className="text-xl ml-6 -mt-1">Explore</span>
-            </a>
-            <a href="#" className="flex rounded-3xl px-3 py-3 navlink">
+            </Link>
+            <Link to="/blog" className="flex rounded-3xl px-3 py-3 navlink">
                 <i className="material-icons-outlined pl-3">article</i>
                 <span className="text-xl ml-6 -mt-1">Blog</span>
-            </a>
-            <a href="#" className="flex rounded-3xl px-3 py-3 navlink">
+            </Link>
+            <Link to="/community" className="flex rounded-3xl px-3 py-3 navlink">
                 <i className="material-icons-outlined pl-3">forums</i>
                 <span className="text-xl -mt-1">Community</span>
-            </a>
-            <a href="#" className="flex rounded-3xl px-3 py-3 navlink">
+            </Link>
+            <Link to="/account" className="flex rounded-3xl px-3 py-3 navlink">
                 <i className="material-icons-outlined pl-3">account_circle</i>
                 <span className="text-xl ml-6 -mt-1">Profile</span>
-            </a>
-            <a href="#" className="flex rounded-3xl px-3 py-3 navlink">
+            </Link>
+            <Link to="/about" className="flex rounded-3xl px-3 py-3 navlink">
                 <i className="material-icons-outlined pl-3">info</i>
                 <span className="text-xl ml-6 -mt-1">About</span>
-            </a>
+            </Link>
         </Fragment>
     )
 }
