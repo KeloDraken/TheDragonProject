d56617236a572b174df86c0fc90b506899477df2
Samkelo <joymama078@gmail.com>
2021-12-19 12:01:00 +0200

    Move sidebar recommendations to global store

--- vetkoek/src/components/Sidebar/index.tsx
+++ vetkoek/src/components/Sidebar/index.tsx
@@ -1,3 +1,4 @@
+import { view } from "@risingstack/react-easy-state";
 import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
@@ -6,30 +7,38 @@ import Footer from "./Footer";
 import SearchForm from "./SearchForm";
 import SidebarCard from "./SidebarCard";
 
-const Sidebar = () => {
-  const [posts, setPost] = useState<Array<object>>([]);
+import { postsList } from "../../store";
+
+const Sidebar = view(() => {
   const [loading, setLoading] = useState<boolean>(true);
 
   const handleGetRecommended = () => {
     const endpoint = "http://127.0.0.1:8000/api/v1/posts/get/recommended/";
 
     axios.get(endpoint).then((response) => {
-      setPost(response.data.results);
+      postsList.data = response.data.results;
       setLoading(false);
     });
   };
 
+  const _posts = postsList.data;
+
   useEffect(() => {
-    handleGetRecommended();
+    if (_posts.length === 0) {
+      handleGetRecommended();
+    }else{
+      setLoading(false)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <View>
       <SearchForm />
-      <SidebarCard loading={loading} posts={posts} cardTitle="For you" />
+      <SidebarCard loading={loading} posts={_posts} cardTitle="For you" />
       <Footer />
     </View>
   );
-};
+});
 
 export default Sidebar;
