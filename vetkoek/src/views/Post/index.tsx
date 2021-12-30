8a6f92f829c811c2d497781dedaedc21d0223b4e
Samkelo <joymama078@gmail.com>
2021-12-30 16:03:54 +0200

    Add loading indicator in post view page

--- vetkoek/src/views/Post/index.tsx
+++ vetkoek/src/views/Post/index.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { ActivityIndicator } from "react-native";
 import { useParams } from "react-router-dom";
 
 import Navbar from "../../components/Navbar";
@@ -9,11 +10,13 @@ import Sidebar from "../../components/Sidebar";
 const PostView = () => {
   let { id } = useParams();
   const [post, setPost] = useState<Array<object>>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const handlePostFetch = () => {
     const endpoint = `http://127.0.0.1:8000/api/v1/posts/get/${id}`;
     axios.get(endpoint).then((response) => {
       setPost(response.data.results);
+      setLoading(false);
     });
   };
 
@@ -29,9 +32,15 @@ const PostView = () => {
       </aside>
 
       <main className="w-full overflow-y-scroll h-screen sticky top-0">
-        {post.map((item: object, index: number) => {
-          return <ViewPost key={index} item={item} />;
-        })}
+        {loading ? (
+          <div className="flex h-screen justify-center self-center align-center">
+            <ActivityIndicator color={"#000"} size={36} />
+          </div>
+        ) : (
+          post.map((item: object, index: number) => {
+            return <ViewPost key={index} item={item} />;
+          })
+        )}
       </main>
 
       <aside className="overflow-y-scroll h-screen hidden lg:block xl:block 2xl:block sticky top-0 w-7/12 pr-7 pl-3">
