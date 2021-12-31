3556f1ff445d46c6dce12b09d784c40a6e7ffead
Samkelo <joymama078@gmail.com>
2021-12-31 08:51:38 +0200

    Create updates list url pattern

--- biltong/config/urls.py
+++ biltong/config/urls.py
@@ -6,6 +6,7 @@ from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/core/", include("core.urls", namespace="core")),
     path("api/v1/users/login/", obtain_jwt_token, name="user-login"),
     path("api/v1/users/", include("core.accounts.urls", namespace="accounts")),
     path("api/v1/posts/", include("core.posts.urls", namespace="posts")),
