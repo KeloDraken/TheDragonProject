ff54b5e8221943edfc91a6fb938fc7a98a0085e6
Samkelo <joymama078@gmail.com>
2021-12-31 08:38:23 +0200

    Show tags user has inputted

--- vetkoek/src/views/Post/AddTags.tsx
+++ vetkoek/src/views/Post/AddTags.tsx
@@ -12,12 +12,15 @@ const AddTags = view(() => {
     postTags.tags = tags;
   };
   return (
-    <div className="w-full justify-center mx-auto px-44">
+    <div className="w-full text-center justify-center mx-auto px-44">
       <h1 className="text-2xl text-center mt-16 mb-5 text-black font-bold">
-        {/* {postTags.tags} */}
         Separate tags using commas
       </h1>
-      <div className="flex justify-center mb-10">
+      <small className="text-black">
+        <span className="font-bold">your tags: </span>
+        {postTags.tags.replaceAll(",", " ")}
+      </small>
+      <div className="flex justify-center mt-4 mb-10">
         <View style={styles.searchForm}>
           <View style={styles.searchIconContainer}>
             <i className="p-1 focus:outline-none text-black focus:shadow-outline material-icons-outlined">
