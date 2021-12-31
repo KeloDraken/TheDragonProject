0c07257240bf8808d5114e4bfe62a8afd94361e9
Samkelo <joymama078@gmail.com>
2021-12-31 08:46:16 +0200

    Create Updates model

--- biltong/core/models.py
+++ biltong/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from utils.helpers import object_id_generator
+
+
+class Update(models.Model):
+    object_id = models.CharField(max_length=30, null=True, blank=True)
+    content = models.CharField(max_length=300, null=False, blank=False)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, **kwargs):
+        self.object_id = object_id_generator(15, Update)
+        return super(Update, self).save(**kwargs)
